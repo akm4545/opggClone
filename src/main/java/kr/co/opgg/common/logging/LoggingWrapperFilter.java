10544f3e007a51a45eb1a90a4ad4251244b54f49
akm4545 <akm4545@naver.com>
2023-03-28 23:08:39 +0900

    로그 설정

---
+++ src/main/java/kr/co/opgg/common/logging/LoggingWrapperFilter.java
@@ -0,0 +1,82 @@
+package kr.co.opgg.common.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class LoggingWrapperFilter extends OncePerRequestFilter {
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long start = System.currentTimeMillis();
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long end = System.currentTimeMillis();
+
+
+        log.info("[{}-{}][{}ms]->[{}]\n[Headers]- {}\n[Request]- {}\n[Response]- {}", (request).getMethod(), getRequestURI(request), (end - start) / 1000.0,
+                responseWrapper.getStatus(), getHeaders(request), getRequestBody(requestWrapper), getResponseBody(responseWrapper));
+    }
+
+    private String getRequestURI(HttpServletRequest request) {
+        String queryString = request.getQueryString();
+        return queryString == null ? request.getRequestURI() : request.getRequestURI() + "?" + queryString;
+    }
+
+    private Map getHeaders(HttpServletRequest request) {
+        Map headerMap = new HashMap<>();
+
+        Enumeration headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = (String) headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                try {
+                    return new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+                } catch (UnsupportedEncodingException e) {
+                    return " - ";
+                }
+            }
+        }
+        return " - ";
+    }
+
+    private String getResponseBody(final HttpServletResponse response) throws IOException {
+        String payload = null;
+        ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                payload = new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+                wrapper.copyBodyToResponse();
+            }
+        }
+        return null == payload ? " - " : (response.getContentType().contains("text/html") ? "WEB PAGE" : payload);
+    }
+
+}
