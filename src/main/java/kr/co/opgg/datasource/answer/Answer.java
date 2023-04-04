a25aa2fda0b5fa9a5725880d7dccf7d93569674b
akm4545 <akm4545@naver.com>
2023-04-04 20:38:18 +0900

    엔티티 연관관계 맺는 중...

--- src/main/java/kr/co/opgg/datasource/answer/Answer.java
+++ src/main/java/kr/co/opgg/datasource/answer/Answer.java
@@ -1,6 +1,8 @@
 package kr.co.opgg.datasource.answer;
 
 import kr.co.opgg.datasource.common.Date;
+import kr.co.opgg.datasource.qna.QNA;
+import kr.co.opgg.datasource.user.User;
 import lombok.*;
 import org.hibernate.annotations.Comment;
 
@@ -20,16 +22,17 @@ public class Answer extends Date {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer answerIdx;
 
-
     @Comment("답변 내용")
     @Column(name = "answer_content")
     private String answerContent;
 
-    @Comment("질문 인덱스")
-    @Column(name = "qna_idx")
-    private Integer qnaIdx;
+    @Comment("질문")
+    @OneToOne(cascade = CascadeType.ALL ,fetch = FetchType.LAZY)
+    @JoinColumn(name = "qna_idx")
+    private QNA qna;
 
-    @Comment("유저 인덱스")
-    @Column(name = "user_idx")
-    private Integer userIdx;
+    @Comment("유저")
+    @ManyToOne(cascade = CascadeType.ALL ,fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx")
+    private User user;
 }
