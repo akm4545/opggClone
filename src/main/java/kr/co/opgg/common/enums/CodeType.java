549b922053f5c82accb63ccc2dc44dbe935b8636
akm4545 <akm4545@naver.com>
2023-04-09 19:58:10 +0900

    FAQ 코드 타입 작성

--- src/main/java/kr/co/opgg/common/enums/CodeType.java
+++ src/main/java/kr/co/opgg/common/enums/CodeType.java
@@ -1,7 +1,12 @@
 package kr.co.opgg.common.enums;
 
 public class CodeType {
-    public enum NoticeType{
-        TEST, TESTTEST
+
+    /*
+    *  FAQ
+    *  [패치노트, 랭킹, 전적, 관전하기/리플레이, 챔피언 분석, 기타]
+    * */
+    public enum FAQType{
+        PATCH, RANKING, RECORD, REPLAY, CHAMPION, ETC
     }
 }
