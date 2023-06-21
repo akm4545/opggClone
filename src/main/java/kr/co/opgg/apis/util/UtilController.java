0b01bb01eccbfa036f36634a7f7035e815d1e2cf
akm4545 <akm4545@naver.com>
2023-06-21 20:45:30 +0900

    html style 태그 변환 controller 작성

--- src/main/java/kr/co/opgg/apis/util/UtilController.java
+++ src/main/java/kr/co/opgg/apis/util/UtilController.java
@@ -30,7 +30,18 @@ public class UtilController {
                 "                                            style=\"position: absolute;top: 50%;left: 4px;transform: translate(-50%, -50%);width: 0;height: 0;border-top: 15px solid transparent;border-left: 27px solid #eeeeee;border-bottom: 15px solid transparent;\">\n" +
                 "                                        </div>\n" +
                 "                                    </div>\n" +
-                "                                </div>";
+                "                                </div>\n" +
+                "                                <div id=\"primis_playerSekindoSPlayer649040c5d7a6d\"\n" +
+                "                                    style=\"overflow: visible; position: fixed; width: 405px; height: 228px; box-shadow: rgba(0, 0, 0, 0.4) 1px 1px 6px 3px; border-radius: 1px; display: block; z-index: 2147483646; left: 10px; bottom: 10px;\">\n" +
+                "                                    <div id=\"Player-Div-SekindoSPlayer649040c5d7a6d\"\n" +
+                "                                        style=\"width: max-content; height: max-content; top: auto; left: auto; position: static; transform: none;\">\n" +
+                "                                        <div id=\"closeContainer\"\n" +
+                "                                            style=\"display: block; height: 21px; width: 100%; position: absolute; pointer-events: none; z-index: 5; top: -21px;\">\n" +
+                "                                            <div id=\"closeIcon\"\n" +
+                "                                                style=\"height: 16px; width: 16px; position: relative; float: left;\">\n" +
+                "                                                <div\n" +
+                "                                                    style=\"pointer-events: auto; width: 60px;height: 60px;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: -1;\">\n" +
+                "                                                </div>";
 
         Integer startIndex = 0;
         Integer endIndex = 0;
@@ -58,7 +69,11 @@ public class UtilController {
             convertTargetHtml = convertTargetHtml.replaceAll(" ", "");
 
             convertHtmlString += convertTargetHtml;
+
+            startIndex = endIndex;
         }
+
+        System.out.println(convertHtmlString);
 
         return ResponseEntity.ok(responseService.getSingleResult(convertHtmlString));
     };
