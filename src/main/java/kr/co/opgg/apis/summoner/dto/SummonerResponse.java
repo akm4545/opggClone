cf7a43887eb8f0ae8af3849979c74236dc82d00a
leesungkyu <lccab2002@gmail.com>
2023-07-04 21:24:01 +0900

    검색한 소환사 데이터 추출하는 부분 구현

--- src/main/java/kr/co/opgg/apis/summoner/dto/SummonerResponse.java
+++ src/main/java/kr/co/opgg/apis/summoner/dto/SummonerResponse.java
@@ -1,10 +1,13 @@
 package kr.co.opgg.apis.summoner.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 @Data
@@ -40,6 +43,7 @@ public class SummonerResponse {
     public static class Match{
         private MetaData metadata;
         private Info info;
+        private Match summonerMatch;
     }
 
     @Getter
@@ -63,6 +67,7 @@ public class SummonerResponse {
         private int deaths; //죽은 수
         private int largestMultiKill; //최대 연속 킬수
         private int kills; //킬수
+        private int assists;
         private int item0; //아이템 칸수
         private int item1;
         private int item2;
@@ -77,5 +82,19 @@ public class SummonerResponse {
         private int summonerLevel;
         private int summoner1Id; //스펠
         private int summoner2Id; //스펠
+    }
+
+    @Getter
+    @ToString
+    public static class MatchResult{
+        private List<Match> matches = new ArrayList<>();
+
+        private List<Map<String, MatchParticipant>> summonerMatches = new ArrayList<>();
+
+        @Builder
+        public MatchResult(List<Match> matches, List<Map<String, MatchParticipant>> summonerMatches){
+            this.matches.addAll(matches);
+            this.summonerMatches.addAll(summonerMatches);
+        }
     }
 }
