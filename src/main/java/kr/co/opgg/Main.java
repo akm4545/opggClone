39ae44d1aed27d3180352e9541c9e3bbee562c32
akm4545 <akm4545@naver.com>
2023-07-19 23:41:09 +0900

    cicd 테스트

--- src/main/java/kr/co/opgg/Main.java
+++ src/main/java/kr/co/opgg/Main.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableScheduling
 @SpringBootApplication
 public class Main {
-    //배포 테스트4
+    //배포 테스트5
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
