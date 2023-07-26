fc91a6038ca43f8a1331c13293f7b2921470bcb8
akm4545 <akm4545@naver.com>
2023-07-26 12:45:16 +0900

    db 접속 url 수정

--- src/main/java/kr/co/opgg/Main.java
+++ src/main/java/kr/co/opgg/Main.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableScheduling
 @SpringBootApplication
 public class Main {
-    //배포 테스트10
+    //배포 테스트11
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
